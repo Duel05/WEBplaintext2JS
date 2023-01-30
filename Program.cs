e1d6a58371e9675d111faf5482fb81902c394e26
DabbingTree <37164157+DuelTheBearded@users.noreply.github.com>
2023-01-29 22:23:41 -0500

    Made project.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿string book_name = args[0];
+
+string[] files = Directory.GetFiles("web/");
+List<List<string>> chapters = new List<List<string>>();
+foreach (string file in files)
+{
+    List<string> chapter = new List<string>();
+    
+    string contents = File.ReadAllText(file);
+    string[] lines = contents.Split(
+        new string[] { "\r\n", "\r", "\n" },
+        StringSplitOptions.None
+    );
+    
+    for (int i = 0; i < lines.Length; i++)
+    {
+        if (i > 1)
+        {
+            chapter.Add(lines[i]);
+        }
+    }
+
+    chapters.Add(chapter);
+}
+
+string js = "const " + book_name + " = [";
+List<string> last = chapters.Last();
+foreach (List<string> chapter in chapters)
+{
+    js += "{";
+    js += "verses: [";
+    int i = -1;
+    string v_last = chapter.Last();
+    foreach (string verse in chapter)
+    {
+        js += "`" + verse + "`";
+        if (!verse.Equals(v_last)) js += ",";
+    }
+    js += "]";
+    js += "}";
+    if (!chapter.Equals(last)) js += ",";
+}
+js += "];";
+File.WriteAllText("output.txt", js);
